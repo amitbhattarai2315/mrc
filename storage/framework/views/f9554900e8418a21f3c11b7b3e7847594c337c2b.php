83eadb9b278aeec4b56fa79a63c067116c820abe
amitbhattarai2315 <72956027+amitbhattarai2315@users.noreply.github.com>
2022-12-07 15:34:21 +0545

    Add files via upload

---
+++ storage/framework/views/f9554900e8418a21f3c11b7b3e7847594c337c2b.php
@@ -0,0 +1,362 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+	    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="fonts/icomoon/style.css">
+
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+	<link rel="shortcut icon" href="logo.png" type="image/x-icon">
+    <title>Mahendra Ratna Campus</title>
+  </head>
+  <body>
+      <!-- Navbar  -->
+    <div class="site-mobile-menu site-navbar-target">
+        <div class="site-mobile-menu-header">
+          <div class="site-mobile-menu-close mt-3">
+            <span class="icon-close2 js-menu-toggle"></span>
+          </div>
+        </div>
+        <div class="site-mobile-menu-body"></div>
+      </div>
+
+
+      <div class="top-bar">
+        <div class="container">
+          <div class="row">
+            <div class="col-12">
+              <a href="#" class=""><span class="mr-2  icon-envelope-open-o"></span> <span class="d-none d-md-inline-block">mahendraratnacampus@gmail.com</span></a>
+              <span class="mx-md-2 d-inline-block"></span>
+              <a href="#" class=""><span class="mr-2  icon-phone"></span> <span class="d-none d-md-inline-block">01-537 1728</span></a>
+
+
+              <div class="float-right">
+
+                <a href="#" class=""><span class="mr-2  icon-twitter"></span> <span class="d-none d-md-inline-block">Twitter</span></a>
+                <span class="mx-md-2 d-inline-block"></span>
+                <a href="https://www.facebook.com/mahendraratnacampustahachal" class=""><span class="mr-2  icon-facebook"></span> <span class="d-none d-md-inline-block">Facebook</span></a>
+
+              </div>
+
+            </div>
+
+          </div>
+
+        </div>
+      </div>
+
+      <header class="site-navbar js-sticky-header site-navbar-target" role="banner">
+
+        <div class="container">
+          <div class="row align-items-center position-relative">
+
+
+            <div class="site-logo">
+				<img src="logo.png" class="image-fluid" height=30px>
+              <a href="index.html" class="text-black"><span class="text-info">MRC</a>
+            </div>
+
+            <div class="col-12">
+              <nav class="site-navigation text-right ml-auto " role="navigation">
+
+                <ul class="site-menu main-menu js-clone-nav ml-auto d-none d-lg-block">
+                  <li><a href="#home-section" class="nav-link">Home</a></li>
+                  <li><a href="http://202.45.146.156/login/index.php" class="nav-link bg-danger text-white">e-Learning</a></li>
+
+
+                  <li class="has-children">
+                    <a href="#about-section" class="nav-link">About Us</a>
+                    <ul class="dropdown arrow-top">
+                      <li><a href="#team-section" class="nav-link">Team</a></li>
+                      <li><a href="#pricing-section" class="nav-link">Notices</a></li>
+                      <li><a href="#faq-section" class="nav-link">FAQ</a></li>
+                      <li class="has-children">
+                        <a href="#">More Links</a>
+                        <ul class="dropdown">
+                          <li><a href="#">Menu One</a></li>
+                          <li><a href="#">Menu Two</a></li>
+                          <li><a href="#">Menu Three</a></li>
+                        </ul>
+                      </li>
+                    </ul>
+                  </li>
+
+                  <li><a href="#why-us-section" class="nav-link">Why Us</a></li>
+
+                  <li><a href="#Downloads" class="nav-link">Downloads</a></li>
+                  <li><a href="#Contact_us" class="nav-link">Contact Us</a></li>
+                <li>
+				<form class="form-inline my-2 my-lg-0">
+					<input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+					<button class="btn btn-outline-info nav-link my-2 my-sm-0" type="submit">Search</button>
+				</form>
+				<li>
+				</ul>
+              </nav>
+
+            </div>
+
+            <div class="toggle-button d-inline-block d-lg-none"><a href="#" class="site-menu-toggle py-5 js-menu-toggle text-black"><span class="icon-menu h3"></span></a></div>
+
+          </div>
+        </div>
+
+      </header>
+	  
+
+   <!-- <div class="hero" style="background-image: url('images/hero_1.jpg');"></div> -->
+  <!-- marquee --> 
+  
+
+<div class="row text-black" >
+<div class="col-2 d-flex flex-row-reverse">
+<h6 >Notices|</h6>
+</div>
+<div class="col-10 ">
+<h6>  <marquee behavior="scroll" scrolldelay="100" scrollamount="4" onmouseover="document.all.test.stop()" onmouseout="document.all.test.start()">Welcome to Mahendra Ratna Campus, Website under Maintainance...</marquee></h6></span>
+</div>
+</div>
+ <!-- image slider  -->
+
+ <section class="section dashboard">
+      <div class="row">
+
+        <div class="col-lg-8">
+<div id="carouselExampleIndicators" class="carousel slide " data-ride="carousel">
+    <ol class="carousel-indicators ">
+        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+		<li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+    </ol>
+    <div class="carousel-inner">
+        <div class="carousel-item active">
+            <img class="d-block w-100 img-responsive " src="slideshow/1.jpg"  height="500px" alt="First slide">
+            <div class="carousel-caption d-none d-md-block">
+                <h5>New Building of MRC</h5>
+                <p>All classes of Bachlore and masters are held in this building</p>
+            </div>
+        </div>
+        <div class="carousel-item">
+            <img class="d-block w-100 img-responsive" src="slideshow/2.jpg" height="500px" alt="Second slide">
+        </div>
+        <div class="carousel-item">
+            <img class="d-block w-100 img-responsive" src="slideshow/3.jpg" height="500px" alt="Third slide">
+        </div>
+		<div class="carousel-item">
+            <img class="d-block w-100 img-responsive" src="slideshow/4.jpg" height="500px" alt="Third slide">
+        </div>
+    </div>
+    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon bg-danger" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+        <span class="carousel-control-next-icon bg-danger" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+    </a>
+</div>
+  
+   
+          
+        </div>
+        <div class="col-lg-4">
+          <div class="card">
+          <!-- News & Updates Traffic -->
+          <div class="card">
+            <div class="card-body pb-0">
+              <h5 class="card-title">News &amp; Updates |<span> View all</span></h5>
+
+              <div class="news">
+                <div class="post-item clearfix">
+                  <img src="upload/notice.jpg" alt="">
+                  <h4><a href="upload/notice.jpg">Admission open</a></h4>
+                  <p>Admission open in BICT and 4 year B.ED <br><span>| Nov2 </span> </p>
+				  
+                </div>
+
+                <div class="post-item clearfix">
+                  <img src="upload/KUScholarship.jpg" alt="">
+                  <h4><a href="upload/KUScholarship.jpg">K.U. School of Edu. Scholarship Notice</a></h4>
+                  <p>K.U. School of Edu. Scholarship Notice</p>
+                </div>
+				
+				<div class="post-item clearfix">
+                  <img src="upload/ADFElitscholarship.jpg" alt="">
+                  <h4><a href="upload/ADFElitscholarship.jpg">ADFElitscholarship.jpg</a></h4>
+                  <p>ADF ELIT Scholarship, Korea Notice</p>
+                </div>
+				
+                
+
+              </div><!-- End sidebar recent posts-->
+<button type="button" class="btn btn-primary"> See More</button>
+            </div>
+          </div><!-- End News & Updates -->
+
+        </div><!-- End Right side columns -->
+
+      </div>
+    </section>
+     
+   <!-- New news -->
+  
+                    
+    <section class="section dashboard">
+      <div class="row">
+	  <!-- FIrst column -->
+        <div class="col-lg-4">
+  <div class="card">
+            <div class="card-body pb-0">
+              <h5 class="card-title">News &amp; Updates |<span> View all</span></h5>
+              <div class="news">
+                <div class="post-item clearfix">
+                  <img src="upload/ADFElitscholarship.jpg" alt="">
+                  <h4><a href="upload/ADFElitscholarship.jpg">ADFElitscholarship.jpg</a></h4>
+                  <p>ADF ELIT Scholarship, Korea Notice</p>
+                </div>
+              </div>
+				<button type="button" class="btn btn-primary"> See More</button>
+            </div>
+          </div>
+  
+  
+		</div>
+		<!-- MIddle column -->
+		<div class="col-lg-4">
+		
+		
+  <div class="card">
+            <div class="card-body pb-0">
+              <h5 class="card-title"></i>Downloads</h5>
+			   <p><a class="text-secondary"href="upload/Application Form Master_Bachelor Degree in Education.pdf" ><i class="fa fa-download" aria-hidden="true"></i> Application Form Master_Bachelor Degree in Education</p>
+              <p><a class="text-secondary"href="upload/सम्पत्ति विवरण फारम.pdf" ><i class="fa fa-download" aria-hidden="true"></i> सम्पत्ति विवरण फारम</a></p>
+			  <p><a class="text-secondary"href="upload/Newsalary.pdf" ><i class="fa fa-download" aria-hidden="true"></i> नया तलब स्केल</p>
+			    <p><a class="text-secondary"href="upload/अन्य स‍स्थामा काम गर्न अनुमति.jpg" ><i class="fa fa-download" aria-hidden="true"></i> शिक्षक/कर्मचारीले अन्य संस्थामा आंशिक रुपमा काम गर्न अनुमति माग गर्ने फाराम</a></p>
+			 				<button type="button" class="btn btn-primary"> See More</button>
+            </div>
+          </div>
+  
+  
+		</div>
+		<!-- Last column -->
+		<div class="col-lg-4">
+		<div class="card">
+		 <div class="card-body pb-0">
+        <h5 class="card-title"></i>Quick Links</h5>
+         <?php $__currentLoopData = $ql; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $q): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+        <p><a class="text-secondary" href="<?php echo e($q->link); ?>" ><?php echo e($q->name); ?></a></p>
+				<?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+        </div>
+        </div>
+      </div>
+	  
+
+  
+  
+  </div>
+  </div>
+  </section>                
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+        
+       
+ 
+
+   <!-- body  -->
+  
+  <!-- footer start  -->
+<div class="card-group">
+   
+  <div class="col-lg-4">
+    <div class="card bg-primary text-white ">
+	 <h5 class="card-title text-center">Contact Us</h5>
+      <iframe class="card-img-top" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14130.385595098505!2d85.30275596586915!3d27.698866503211335!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xced44e5b8cbff98d!2sMahendra-Ratna%20Campus!5e0!3m2!1sen!2snp!4v1660202110875!5m2!1sen!2snp" width="300" height="400" style="border:0;"
+	  allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+      <div class="card-body">
+        <p>Mahendra Ratna Campus</p>
+		<p>Tahachal, Kathmandu</br>
+		Phone Number:-01-537 1728</br>
+		Email:- mahendraratnacampus@gmail.com, info@mrc.tu.edu.np </br>
+		 
+		<a href ="https://mrctu.edu.np/" style="color: white;">Website:- mrctu.edu.np</a></p>
+      </div>
+    </div>
+  </div>
+  
+   <div class="col-lg-4">
+    <div class="card bg-primary text-white">
+      <div class="card-body text-white">
+        <h5 class="card-title text-center">Follow Us</h5>
+        <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fmahendraratnacampustahachal&tabs=timeline&width=350&height=550&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" width="400" height="570" style="border:none;overflow:hidden" scrolling="no" frameborder="0"
+		allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
+      
+	  </div>
+    </div>
+  </div>
+  
+  
+  
+  <div class="col-lg-4">
+    <div class="card bg-primary text-white border-secondary">
+      <div class="card-body ">
+        <h5 class="card-title text-center">Gallary </h5>
+        <p class="card-text">
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br><br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br><br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		</p>
+      </div>
+    </div>
+  </div>
+ 
+ 
+</div>
+</div>
+<h6 class="font-7 text-white-777 ml-3 mr-3 text-center bg-primary text-white" style="font-size: 20px; color: gray"><b>Copyright ©2022 Mahendra Ratna Campus. All Rights Reserved</b></h6>
+ 
+
+ <!-- javaScript -->
+ <script src="https://use.fontawesome.com/0fa96c6c04.js"></script>
+ 
+ <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+ 
+ 
+ 
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery.sticky.js"></script>
+    <script src="js/main.js"></script>
+	</body>
+</html><?php /**PATH C:\xampp\htdocs\mrc\resources\views/test.blade.php ENDPATH**/ ?>
